09b77e3e8c1e836e776564c8089431d847927570
soheilnazari2001 <s.nazari80820@gmail.com>
2023-04-03 19:30:32 +0430

    add Reader Class add Token class Start Scanner Class

--- src/scanner.py
+++ src/scanner.py
@@ -17,3 +17,79 @@ class SymbolTable:
         for i, symbol in enumerate(self.symbols):
             s += f'{str(i + 1) + ".":<5} {symbol}\n'
         return s
+
+
+class Reader:
+    def __init__(self, file_name):
+        self.line_number = 0
+        self.file_name = file_name
+        self.file = open(file_name, 'r')
+        self.line = self.readline()
+        self.char_index = 0
+
+    def get_char(self):
+        if self.char_index == len(self.line):
+            self.line = self.readline()
+            self.char_index = 0
+            if not self.line:
+                return None
+        c = self.line[self.char_index]
+        self.char_index += 1
+        return c
+
+    def get_line_number(self):
+        return self.line_number
+
+    def close(self):
+        self.file.close()
+
+    def open(self):
+        self.file = open(self.file_name, 'r')
+
+    def readline(self):
+        line = self.file.readline()
+        if line:
+            self.line_number += 1
+        return line
+
+
+class Token:
+    def __init__(self, token_type, token_value):
+        self.token_type = token_type
+        self.token_value = token_value
+
+    def __str__(self):
+        return f'{self.token_type} {self.token_value}'
+
+    def get_type(self):
+        return self.token_type
+
+    def get_value(self):
+        return self.token_value
+
+    def set_type(self, token_type):
+        self.token_type = token_type
+
+    def set_value(self, token_value):
+        self.token_value = token_value
+
+
+class Scanner:
+    def __init__(self):
+        self.symbol_table = SymbolTable()
+        self.tokens = []
+        self.text = ''
+        self.reader = None
+
+    def set_reader(self, reader):
+        self.reader = reader
+
+    def get_token(self):
+        return self.tokens.pop(0)
+
+    def get_symbol_table(self):
+        return self.symbol_table
+
+
+
+
