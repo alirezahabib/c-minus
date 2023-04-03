09e534533f1f3221ac2a965825f941e4d13219eb
Alireza Habibzadeh <alirezahabib80@gmail.com>
2023-04-03 15:00:24 +0330

    Added comments for readability

--- src/scanner.py
+++ src/scanner.py
@@ -3,15 +3,17 @@ keywords = ['break', 'else', 'if', 'int', 'repeat', 'return', 'until', 'void']
 
 class SymbolTable:
     def __init__(self):
+        # Here dictionary acts as an ordered set
         self.symbols = dict.fromkeys(keywords)
 
     def add_symbol(self, symbol):
+        # Symbols is a dictionary, so no need to check if symbol is already in the table
         self.symbols[symbol] = None
 
+    # Used to create symbol_table.txt file, use str(symbol_table) to get the string
+    # Or use print(symbol_table) to print the table
     def __str__(self):
         s = ''
         for i, symbol in enumerate(self.symbols):
             s += f'{str(i + 1) + ".":<5} {symbol}\n'
         return s
-
-
