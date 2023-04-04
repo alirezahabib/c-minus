2e6dfe131867fa8d558807c5463f3efa09b4d650
Alireza Habibzadeh <alirezahabib80@gmail.com>
2023-04-04 11:53:25 +0330

    Add test file

---
+++ test/test1.py
@@ -0,0 +1,13 @@
+import unittest
+
+from Compiler import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+        SymbolTable()
+
+
+if __name__ == '__main__':
+    unittest.main()
